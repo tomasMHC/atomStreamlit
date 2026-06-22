2271686adf8cf81cccd9e32b796e5de608942b99
Tomas Porubsky <Tomas.Porubsky@onsemi.com>
2026-06-22 11:08:02 +0200

    update dph

--- cart.py
+++ cart.py
@@ -6,7 +6,7 @@ import requests
 import unicodedata
 from pathlib import Path
 
-st.set_page_config(page_title="PharmaGroup katalóg", layout="None")
+st.set_page_config(page_title="PharmaGroup katalóg")
 
 # =========================================================
 # Session state initialization
